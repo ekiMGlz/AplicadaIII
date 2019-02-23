49f74ee91fb2ea79f3a124daa795bde700ab9387
ekiMGlz <mike.glz.96@gmail.com>
2019-02-23 17:21:15 -0600

    script para generar graficas de scatter entre attributos normalizados

--- Wisconsin_Cancer/parser.py
+++ Wisconsin_Cancer/parser.py
@@ -4,14 +4,24 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv("data/wdbc.data")
-A = df.iloc[:, 2:32].values
 
 # Normalizar la matriz de datos
-for col in A.T:
-    mu = np.mean(col)
-    sigma = np.std(col)
-    col -= mu
-    col /= sigma
+for col in df.columns[2:]:
+    mu = np.mean(df[col])
+    sigma = np.std(df[col])
+    df[col] = df[col].apply(lambda x: (x-mu)/sigma)
 
-sns.pairplot(df, hue="Diagnosis", vars=list(df)[2:32:10])
-plt.show()
+# Scatter Matrix por nucleo de cada variable
+sns.pairplot(df, hue="Diagnosis", vars=list(df)[2:12])
+plt.savefig("graphs/scatter_matrix_n1.png")
+
+sns.pairplot(df, hue="Diagnosis", vars=list(df)[12:22])
+plt.savefig("graphs/scatter_matrix_n2.png")
+
+sns.pairplot(df, hue="Diagnosis", vars=list(df)[22:32])
+plt.savefig("graphs/scatter_matrix_n3.png")
+
+# Scatter Matrix por variable de cada nucleo
+for i in range(2, 12):
+    sns.pairplot(df, hue="Diagnosis", vars=list(df)[i::10])
+    plt.savefig("graphs/scatter_matrix_v" + str(i - 1) + ".png")
